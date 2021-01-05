775b6aeaf9a3e64771a78177d5280f30dbd8ab21
grimmigerFuchs <dominic.grimm@gmail.com>
2021-01-05 22:18:46 +0100

    Changed setup.py to match the Python Packaging standards

--- setup.py
+++ setup.py
@@ -2,16 +2,23 @@
 
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
-    name="pyticles",
+    name="pyticles-grimmigerFuchs",
     version="0.1.1",
     author="grimmigerFuchs",
-    author_email="grimmigerfuchs@gmail.com",
-    url="https://github.com/grimmigerfuchs/pyticles",
-    license="MIT",
+    author_email="author@example.com",
     description="A short library for easy to use particles in Pygame based on DaFluffyPotato's particle system.",
-    packages=["pyticles"],
-    install_requires=[
-        "pygame~=2.0.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/grimmigerfuchs/pyticles",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
 )
