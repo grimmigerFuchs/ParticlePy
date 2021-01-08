cc0bc37e8fe358d5fa632278124e34f4a2a177d2
grimmigerFuchs <dominic.grimm@gmail.com>
2021-01-08 18:48:22 +0100

    🔥 Removed code from math.py

--- particlepy/math.py
+++
@@ -1,9 +0,0 @@
-# math.py
-
-import particlepy.particle
-
-
-def fade_to_color(particle: particlepy.particle.BaseParticle, wanted_color):
-    return (particle.start_color[0] + (wanted_color[0] - particle.start_color[0]) * particle.twisted_progress,
-            particle.start_color[1] + (wanted_color[1] - particle.start_color[1]) * particle.twisted_progress,
-            particle.start_color[2] + (wanted_color[2] - particle.start_color[2]) * particle.twisted_progress)
