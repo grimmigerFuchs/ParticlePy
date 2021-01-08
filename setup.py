3cbaf28b8c9d51b52ae18e376059e2f50d8528c5
grimmigerFuchs <dominic.grimm@gmail.com>
2021-01-08 13:31:04 +0100

    Added pygame to install_requires

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/grimmigerfuchs/particlepy",
     packages=setuptools.find_packages(),
+    install_requires=["pygame~=2.0.1"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
